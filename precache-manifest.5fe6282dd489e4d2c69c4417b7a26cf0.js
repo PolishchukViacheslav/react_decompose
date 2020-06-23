6d4546711f728bb6e9bbcfeb096fe5f667391e38
Slava Polishchuk <dizerllc@gmail.com>
2020-06-23 20:01:24 +0300

    Updates

---
+++ precache-manifest.5fe6282dd489e4d2c69c4417b7a26cf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d2e9e34fb4d985ff497028a24eeaa17",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2bf0e75afc4a0094bd11",
+    "url": "./static/css/main.b3a87b74.chunk.css"
+  },
+  {
+    "revision": "b0dff7fd6bb18487afb8",
+    "url": "./static/js/2.0ce2c40f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0ce2c40f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bf0e75afc4a0094bd11",
+    "url": "./static/js/main.e63acf05.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
